48def6b33f486e746f90b7564afb63b9e16cf29f
Pham Huu Dang Nhat <phamhuudangnhat@gmail.com>
2014-04-02 21:47:27 +0700

    added main.cpp and moved MB1-libs

---
+++ helloHA/README.md
@@ -0,0 +1,4 @@
+MBoard1-libs
+============
+
+Libraries (in C++) for HLib's MBoard-1.
---
+++ helloHA/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include "MB1_System.h"
+
+int main (void){
+	uint32_t aCount;
+	
+	while (1){
+		MB1_RGB_red.toggle ();
+		delay_ms (1000)
+	}
+	
+	return 0;
+}
+
+
+
