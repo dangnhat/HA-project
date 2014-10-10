d5d51f00ef579f7e8b9a49645f2cae2004c7fc62
nvhien1992 <nvhien1992@gmail.com>
2014-10-10 15:04:04 +0700

    cleanup code in main.cpp

--- apps/ha_sample/main.cpp
+++ apps/ha_sample/main.cpp
@@ -1,118 +1,336 @@
-/*
- * Copyright (C) 2014 Hamburg University of Applied Sciences (HAW)
- * Copyright (C) 2014 Ho Chi Minh University of Technology (HCMUT)
- *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License. See the file LICENSE in the top level directory for more
- * details.
- */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-/**
- * @file        main.cpp
- * @brief       Demonstration of mixed c++ and c user application with pure c RIOT
- *              - mixing of c and c++ source to test name mangling
- *              - introducing a namespace to declarative block, avoiding to qualify calls, e.g. std::vector
- *              - using private and public member functions, e.g. 'cpp_obj.greet()' cannot be accessed from main.cpp
- *              - overloading of function 'cpp_obj.say_hello(...)' for 'none', 'int' or 'float'
- *              - demonstration of templated c++ container 'std::vector'
- *              - usage of iterator to access elements of the container type
- *
- * @author      Martin Landsmann <martin.landsmann@haw-hamburg.de>
- * @author      DangNhat Pham-Huu <51002279@hcmut.edu.vn>
- */
-
-/*
- * all included headers defining c functions, i.e. all RIOT functions, must be marked as extern "C"
- */
 extern "C" {
+#include "transceiver.h"
+#include "board_uart0.h"
 #include "thread.h"
+#include "board.h"
+#include "hwtimer.h"
+#include "vtimer.h"
+#include "msg.h"
+#include "cc110x_ng.h"
 
-#include "c_functions.h"
+#include "cc110x_reconfig.h"
 }
 
-#include <cstdio>
-#include <vector>
-#include "cpp_class.h"
 #include "MB1_System.h"
 
-using namespace std;
+/* definitions */
+#define RECV_HANDLER_STACK_SIZE    (KERNEL_CONF_STACKSIZE_DEFAULT + KERNEL_CONF_STACKSIZE_PRINTF)
+#define RCV_BUFFER_SIZE     (64)
 
-/* thread's stack */
-char threadA_stack[KERNEL_CONF_STACKSIZE_MAIN];
+#define RSSI_OFFSET	(74)
 
-/* thread's function */
-void *threadA_func(void *arg);
+#define NODE_ID	(1)
 
-/* main */
-int main()
+typedef enum
+    : uint8_t {
+        set_rgb_on = 0x01,
+    set_rgb_off = 0x02,
+    set_rgb_value = 0x03, // 3 values follow
+    blink_rgb = 0x04,
+    toggle_rgb = 0x05,
+    set_ledw_on = 0x06,
+    set_ledw_off = 0x07,
+    set_ledw_value = 0x08, // 1 value follows
+    blink_ledw = 0x09,
+    toggle_ledw = 0x0A,
+} command_t;
+
+const char command_string[][128] { "", "set_rgb_on", "set_rgb_off",
+        "set_rgb_value", "blink_rgb", "toggle_rgb", "set_ledw_on",
+        "set_ledw_off", "set_ledw_value", "blink_ledw", "toggle_ledw", };
+
+/* RGB color */
+const uint8_t RGBColors[15][3] = { { 0, 0, 0 }, // black
+        { 255, 0, 0 }, // red
+        { 0, 255, 0 }, // green
+        { 0, 0, 255 }, // blue
+        { 255, 255, 0 }, // yellow
+        { 0, 255, 255 }, // aqua
+        { 255, 0, 255 }, // magenta
+        { 192, 192, 192 }, // silver
+        { 128, 128, 128 }, // gray
+        { 128, 0, 0 }, // maroon
+        { 128, 128, 0 }, // olive
+        { 0, 128, 0 }, // green
+        { 128, 0, 128 }, //purple
+        { 0, 128, 128 }, // teal
+        { 0, 0, 128 } // navy
+};
+
+/* global vars */
+static msg_t mesg;
+static transceiver_command_t tcmd;
+static radio_packet_t p;
+
+static bool rgb_state = 0;
+static uint8_t ledw_duty = 0;
+
+static int taskled_pid;
+
+/* Send data buffer */
+uint8_t send_data_buffer[CC1100_MAX_DATA_LENGTH];
+
+/* recv_handler stack */
+char recv_handler_stack_buffer[RECV_HANDLER_STACK_SIZE];
+
+/* taskled_stack */
+char taskled_stack[KERNEL_CONF_STACKSIZE_PRINTF];
+
+/* recv_handler queue */
+msg_t msg_q[RCV_BUFFER_SIZE];
+
+void* recv_handler(void*);
+
+static bool blink_led = false;
+void* taskled(void*);
+
+int cal_rssi_dbm(uint8_t rssi_dec);
+
+int main(void)
 {
+    int recv_handler_pid;
+    uint8_t color_id = 0;
 
-    /* Init MBoard-1 system */
     MB1_system_init();
 
-    printf("\n************ RIOT, C++ and MBoard-1 demo program ***********\n");
-    printf("\n");
+    recv_handler_pid = thread_create(recv_handler_stack_buffer,
+            RECV_HANDLER_STACK_SIZE, PRIORITY_MAIN - 2, CREATE_WOUT_YIELD,
+            recv_handler, NULL, "Receiving handler");
+    transceiver_init(TRANSCEIVER_CC1100);
+    transceiver_start();
+    cc110x_reconfig();
+    transceiver_register(TRANSCEIVER_CC1100, recv_handler_pid);
 
-    /* create thread A */
-    thread_create(threadA_stack, sizeof(threadA_stack), 0, CREATE_WOUT_YIELD,
-            threadA_func, NULL, "thread A");
+    taskled_pid = thread_create(taskled_stack, KERNEL_CONF_STACKSIZE_PRINTF,
+            PRIORITY_MAIN - 1, CREATE_WOUT_YIELD, taskled, NULL,
+            "Task blink led");
 
-    printf("******** Hello, you're in thread %s ********\n",
-            thread_getname(thread_getpid()));
-    printf("We'll test C++ class and methods here!\n");
+    /* get btn and send data */
+    msg_t rep;
+    while (1) {
+        if (MB1_usrBtn0.pressedKey_get() == Btn_ns::newKey) {
+            printf("UsrBtn0::newKey\n");
 
-    cpp_class cpp_obj;
-    printf("\n-= Test overloading functions =-\n");
-    cpp_obj.say_hello();
-    cpp_obj.say_hello(42);
-    cpp_obj.say_hello(3.141592f);
+            /* send data */
+            mesg.type = SND_PKT;
+            mesg.content.ptr = (char*) &tcmd;
 
-    printf("\n-= Test namespace =-\n");
-    printf("typing std::vector is obsolete when 'using namespace std;'\n");
-    vector<int> vInts;
-    vInts.push_back(1);
-    vInts.push_back(3);
-    vInts.push_back(2);
-    printf("The vector vInts has been filled with %d numbers.\n", vInts.size());
+            tcmd.transceivers = TRANSCEIVER_CC1100;
+            tcmd.data = &p;
 
-    printf("\n-= Test iterator =-\n");
-    printf("The content of vInts = { ");
+            p.length = 1;
+            p.dst = 0;
 
-    for (vector<int>::iterator it = vInts.begin(); it != vInts.end(); ++it) {
-        printf("%d ", *(it));
-    }
+#if (NODE_ID == 1)
+            send_data_buffer[0] = toggle_rgb;
+#else
+            send_data_buffer[0] = toggle_ledw;
+#endif
+            p.data = send_data_buffer;
 
-    printf("}\n");
+            msg_send_receive(&mesg, &rep, transceiver_pid);
+            printf("rep, transceiver_id %d, type %d\n", rep.sender_pid,
+                    rep.type);
+        }
 
-    return 0;
+        if (MB1_usrBtn0.pressedKey_get() == Btn_ns::newLongKey) {
+            printf("UsrBtn0::newLongKey\n");
+            /* send data */
+            mesg.type = SND_PKT;
+            mesg.content.ptr = (char*) &tcmd;
+
+            tcmd.transceivers = TRANSCEIVER_CC1100;
+            tcmd.data = &p;
+
+            p.length = 1;
+            p.dst = 0;
+
+#if (NODE_ID == 1)
+            send_data_buffer[0] = blink_rgb;
+#else
+            send_data_buffer[0] = blink_ledw;
+#endif
+            p.data = send_data_buffer;
+
+            msg_send_receive(&mesg, &rep, transceiver_pid);
+            printf("rep, transceiver_id %d, type %d\n", rep.sender_pid,
+                    rep.type);
+
+            while (MB1_usrBtn0.isStillLongPressed() == true) {
+                ;
+            }
+
+            /* send off command */
+#if (NODE_ID == 1)
+            send_data_buffer[0] = set_rgb_off;
+#else
+            send_data_buffer[0] = set_ledw_off;
+#endif
+
+            msg_send_receive(&mesg, &rep, transceiver_pid);
+            printf("rep, transceiver_id %d, type %d\n", rep.sender_pid,
+                    rep.type);
+        }
+
+        if (MB1_usrBtn1.pressedKey_get() == Btn_ns::newKey) {
+            printf("UsrBtn1::newKey\n");
+            /* send data */
+            mesg.type = SND_PKT;
+            mesg.content.ptr = (char*) &tcmd;
+
+            tcmd.transceivers = TRANSCEIVER_CC1100;
+            tcmd.data = &p;
+
+            p.dst = 0;
+
+#if (NODE_ID == 1)
+            p.length = 4;
+            send_data_buffer[0] = set_rgb_value;
+            send_data_buffer[1] = RGBColors[color_id][0];
+            send_data_buffer[2] = RGBColors[color_id][1];
+            send_data_buffer[3] = RGBColors[color_id][2];
+            color_id = (color_id + 1) % 15;
+            printf("%d\n", color_id);
+
+#else
+            p.length = 2;
+            send_data_buffer[0] = set_ledw_value;
+            send_data_buffer[1] = ledw_value;
+            ledw_value = 1;
+#endif
+
+            p.data = send_data_buffer;
+
+            msg_send_receive(&mesg, &rep, transceiver_pid);
+            printf("rep, transceiver_id %d, type %d\n", rep.sender_pid,
+                    rep.type);
+        }
+    } // end while
 }
 
-/* thread A function implemetation */
-void *threadA_func(void *)
+void* recv_handler(void*)
 {
-    int day = 13, month = 6, year = 2014;
-    int ret_day;
-    char day_of_week_table[][32] = { "Sunday", "Monday", "Tuesday", "Wednesday",
-            "Thursday", "Friday", "Saturday" };
+    msg_t m;
+    radio_packet_t *p;
+    uint8_t count;
+    printf("RecvHandler\n");
 
-    printf("\n******** Hello, now you're in %s ********\n",
-            thread_getname(thread_getpid()));
-    printf("We'll test some C functions here!\n");
+    ledw_duty = 0;
 
-    printf("\n-= hello function =-\n");
-    hello();
+    msg_init_queue(msg_q, RCV_BUFFER_SIZE);
 
-    printf("\n-= day_of_week function =-\n");
+    while (1) {
+        msg_receive(&m);
+        printf("m.type: %d\n", m.type);
+        if (m.type == PKT_PENDING) {
+            p = (radio_packet_t*) m.content.ptr;
+            printf("Packet waiting, process %p...\n", p);
+            printf("\tLength:\t%u\n", p->length);
+            printf("\tSrc:\t%u\n", p->src);
+            printf("\tDst:\t%u\n", p->dst);
+            printf("\tLQI:\t%u\n", p->lqi);
+            printf("\tRSSI:\t%d dBm\n", cal_rssi_dbm(p->rssi));
 
-    printf("day %d, month %d, year %d is ", day, month, year);
+            p->processing--;
 
-    ret_day = day_of_week(day, month, year);
-    if (ret_day >= 0) {
-        printf("%s\n", day_of_week_table[ret_day]);
+            /* process command */
+            printf("Received command %s\n", command_string[p->data[0]]);
+
+            switch (p->data[0]) {
+            case set_rgb_on:
+                printf("set_rgb_on\n");
+                rgb_state = 1;
+                break;
+
+            case set_rgb_off:
+                printf("set_rgb_off\n");
+                rgb_state = 0;
+
+                blink_led = false;
+                break;
+
+            case set_rgb_value:
+                printf("Values %d %d %d\n", p->data[1], p->data[2], p->data[3]);
+                break;
+
+            case blink_rgb:
+                printf("blink_rgb\n");
+                blink_led = true;
+                thread_wakeup(taskled_pid);
+                break;
+
+            case toggle_rgb:
+                printf("toggle_rgb\n");
+                if (rgb_state == 0) {
+                    rgb_state = 1;
+                } else {
+                    rgb_state = 0;
+                }
+                break;
+
+            case set_ledw_on:
+                printf("set_ledw_on\n");
+                break;
+
+            case set_ledw_off:
+                printf("set_ledw_off\n");
+                blink_led = false;
+                break;
+
+            case set_ledw_value:
+                printf("Inc Value %d\n", p->data[1]);
+                break;
+
+            case blink_ledw:
+                printf("blink_ledw\n");
+                blink_led = true;
+                thread_wakeup(taskled_pid);
+                break;
+
+            case toggle_ledw:
+                printf("toggle_ledw\n");
+                break;
+
+            }
+        } else if (m.type == ENOBUFFER) {
+            puts("Transceiver buffer full");
+        } else {
+            puts("Unknown packet received");
+        }
     }
 
-    printf(
-            "\nThis demo ends here, press Ctrl-C to exit (if you're on native)!\n");
+    return NULL;
+}
+
+void* taskled(void*)
+{
+    uint16_t count;
+    printf("taskled\n");
+
+    while (1) {
+        if (blink_led == true) {
+        } else {
+            thread_sleep();
+        }
+    }
 
     return NULL;
+}
+
+int cal_rssi_dbm(uint8_t rssi_dec)
+{
+    int rssi_dBm;
+
+    if (rssi_dec >= 128) {
+        rssi_dBm = (int) ((int) (rssi_dec - 256) / 2) - RSSI_OFFSET;
+    } else {
+        rssi_dBm = (rssi_dec / 2) - RSSI_OFFSET;
+    }
+
+    return rssi_dBm;
 }
