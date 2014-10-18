862a69bb770199fc8260a237ce727103ae7799a9
nvhien1992 <nvhien1992@gmail.com>
2014-10-18 16:30:37 +0700

    EXTI lib had been changed->modified driver

--- libs/RIOT-libs/inc/cc110x_driver.h
+++ libs/RIOT-libs/inc/cc110x_driver.h
@@ -20,16 +20,11 @@
 
 #include "MB1_System.h"
 
-#define GDO0__PORT	GPIOC
-#define GDO0__PIN	GPIO_Pin_7
-#define GDO2__PORT	GPIOC
-#define GDO2__PIN	GPIO_Pin_11
+#define GDO0_PIN_SRC    7
+#define GDO2_PIN_SRC    11
 
-#define	SPI_PORT	GPIOA
-#define	NSS_PIN		GPIO_Pin_4
-#define	SCLK_PIN	GPIO_Pin_5
-#define MISO_PIN	GPIO_Pin_6
-#define MOSI_PIN	GPIO_Pin_7
+#define	SPI_PORT        GPIOA
+#define	NSS_PIN         GPIO_Pin_4
 
 #define CC110x_MISO_LOW_RETRY        (100)        // max. retries for MISO to go low
 #define CC110x_MISO_LOW_COUNT        (2700)        // loop count (timeout ~ 500 us) to wait
--- libs/RIOT-libs/src/cc110x_driver.cpp
+++ libs/RIOT-libs/src/cc110x_driver.cpp
@@ -29,12 +29,21 @@ extern "C" {
 
 #include "cc110x_driver.h"
 
-#define CC110x_GET_GDO0()			(GPIO_ReadInputDataBit(GDO0__PORT, GDO0__PIN))
-#define CC110x_GET_GDO2()			(GPIO_ReadInputDataBit(GDO2__PORT, GDO2__PIN))
-#define CC110x_GET_GDO1_MISO()		(MB1_SPI1.misc_MISO_read())
+/* Global vars changed if MB1 system has been changed */
+gpio *gdo0 = &MB1_EXTI7;
+gpio *gdo2 = &MB1_EXTI11;
+SPI *spi_x = &MB1_SPI1;
+ISRMgr *isr_mgr = &MB1_ISRs;
 
-#define SPI_SELECT()        (MB1_SPI1.SM_device_select(SPI_ns::cc1101_1))
-#define SPI_UNSELECT()      (MB1_SPI1.SM_device_deselect(SPI_ns::cc1101_1))
+ISRMgr_ns::ISR_t gdo0_isr_type = ISRMgr_ns::ISRMgr_EXTI7;
+ISRMgr_ns::ISR_t gdo2_isr_type = ISRMgr_ns::ISRMgr_EXTI11;
+
+#define CC110x_GET_GDO0()           (gdo0->gpio_read())
+#define CC110x_GET_GDO2()           (gdo2->gpio_read())
+#define CC110x_GET_GDO1_MISO()      (spi_x->misc_MISO_read())
+
+#define SPI_SELECT()        (spi_x->SM_device_select(SPI_ns::cc1101_1))
+#define SPI_UNSELECT()      (spi_x->SM_device_deselect(SPI_ns::cc1101_1))
 
 static void gdo_init_interrupt(void);
 static void nss_init(void);
@@ -56,7 +65,7 @@ int cc110x_get_gdo2(void)
 
 static void nss_init(void)
 {
-    MB1_SPI1.SM_numOfSSLines_set(1); //There is 1 chip select line on SPI1.
+    spi_x->SM_numOfSSLines_set(1); //There is 1 chip select line on SPI1.
 
     SPI_ns::SM_GPIOParams_s params_struct;
 
@@ -64,10 +73,10 @@ static void nss_init(void)
     params_struct.GPIO_pin = NSS_PIN;
     params_struct.GPIO_clk = RCC_APB2Periph_GPIOA;
     params_struct.ssLine = 0;
-    MB1_SPI1.SM_GPIO_set(&params_struct);
+    spi_x->SM_GPIO_set(&params_struct);
 
-    MB1_SPI1.SM_deviceToDecoder_set(SPI_ns::cc1101_1, 0);
-    MB1_SPI1.SM_deviceToDecoder_set(SPI_ns::allFree, 1);
+    spi_x->SM_deviceToDecoder_set(SPI_ns::cc1101_1, 0);
+    spi_x->SM_deviceToDecoder_set(SPI_ns::allFree, 1);
 }
 
 void cc110x_spi_init(void)
@@ -86,25 +95,23 @@ void cc110x_spi_init(void)
     params_struct.crcPoly = 7;
     params_struct.nss = SPI_NSS_Soft;
 
-    MB1_SPI1.init(&params_struct);
-    MB1_SPI1.SM_device_attach(SPI_ns::cc1101_1);
+    spi_x->init(&params_struct);
+    spi_x->SM_device_attach(SPI_ns::cc1101_1);
 }
 
 uint8_t cc110x_txrx(uint8_t value)
 {
-    return MB1_SPI1.M2F_sendAndGet_blocking(SPI_ns::cc1101_1, value);
+    return spi_x->M2F_sendAndGet_blocking(SPI_ns::cc1101_1, value);
 }
 
 void cc110x_spi_cs(void)
 {
     SPI_SELECT();
-//    printf("cs: %d\n", GPIO_ReadInputDataBit(SPI_PORT, NSS_PIN));
 }
 
 void cc110x_spi_unselect(void)
 {
     SPI_UNSELECT();
-//    printf("cus: %d\n", GPIO_ReadInputDataBit(SPI_PORT, NSS_PIN));
 }
 
 void cc110x_spi_select(void)
@@ -141,45 +148,46 @@ void cc110x_after_send(void)
 
 void cc110x_gdo0_enable(void)
 {
-    MB1_EXTI7.exti_line_enable();
+    gdo0->exti_line_enable();
 }
 
 void cc110x_gdo2_enable(void)
 {
-    MB1_EXTI11.exti_line_enable();
+    gdo2->exti_line_enable();
 }
 
 void cc110x_gdo0_disable(void)
 {
-    MB1_EXTI7.exti_line_disable();
+    gdo0->exti_line_disable();
 }
 
 void cc110x_gdo2_disable(void)
 {
-    MB1_EXTI11.exti_line_disable();
+    gdo2->exti_line_disable();
 }
 
 static void gdo_init_interrupt(void)
 {
-    exti_ns::exti_params_t params_struct;
+    gpio_ns::gpio_params_t params_struct;
 
     /* Initialize interrupt GDO0 */
-    params_struct.port = exti_ns::port_C;
-    params_struct.mode = exti_ns::floating;
-    params_struct.trigger = exti_ns::rising_edge;
-    MB1_EXTI7.exti_init(&params_struct);
+    params_struct.port = gpio_ns::port_C;
+    params_struct.pin = GDO0_PIN_SRC;
+    params_struct.mode = gpio_ns::in_floating;
+    gdo0->gpio_init(&params_struct);
+    gdo0->exti_init(gpio_ns::rising_edge);
 
     /* Initialize interrupt GDO2 */
-    params_struct.trigger = exti_ns::falling_edge;
-    MB1_EXTI11.exti_init(&params_struct);
+    params_struct.pin = GDO2_PIN_SRC;
+    gdo2->gpio_init(&params_struct);
+    gdo2->exti_init(gpio_ns::falling_edge);
 
-    MB1_ISRs.subISR_assign(ISRMgr_ns::ISRMgr_EXTI7, &cc110x_gdo0_irq);
-    MB1_ISRs.subISR_assign(ISRMgr_ns::ISRMgr_EXTI11, &cc110x_gdo2_irq);
+    isr_mgr->subISR_assign(gdo0_isr_type, &cc110x_gdo0_irq);
+    isr_mgr->subISR_assign(gdo2_isr_type, &cc110x_gdo2_irq);
 }
 
 void cc110x_init_interrupts(void)
 {
     gdo_init_interrupt();
-//    printf("init int\n");
 }
 
