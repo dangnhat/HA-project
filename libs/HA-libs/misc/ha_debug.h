8208359935dd94ad67cc131748ca84643218ee9b
Pham Huu Dang Nhat <phamhuudangnhat@gmail.com>
2014-11-06 14:27:21 +0700

    ha-libs: added ha_debug.h

---
+++ libs/HA-libs/misc/ha_debug.h
@@ -0,0 +1,24 @@
+/**
+ * @file ha_debug.h
+ * @author  Pham Huu Dang Nhat  <phamhuudangnhat@gmail.com>.
+ * @version 1.0
+ * @date 6-Nov-2014
+ * @brief This contains marco for debug and notify.
+ */
+
+#ifndef HA_DEBUG_H_
+#define HA_DEBUG_H_
+
+#if HA_NOTIFICATION
+#define HA_NOTIFY(...) printf(__VA_ARGS__)
+#else
+#define HA_NOTIFY(...)
+#endif
+
+#if HA_DEBUG_EN
+#define HA_DEBUG(...) printf(__VA_ARGS__)
+#else
+#define HA_DEBUG(...)
+#endif
+
+#endif /* HA_DEBUG_H_ */
