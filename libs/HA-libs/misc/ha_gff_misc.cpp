b36785d4cddb60be6bedba02701d7428b6aaab2a
Pham Huu Dang Nhat <phamhuudangnhat@gmail.com>
2014-11-17 11:41:26 +0700

    HA misc: added device id to name function

--- libs/HA-libs/misc/ha_gff_misc.cpp
+++ libs/HA-libs/misc/ha_gff_misc.cpp
@@ -7,8 +7,10 @@
  */
 
 #include <stdint.h>
+#include <stddef.h>
 
 #include "ha_gff_misc.h"
+#include "device_id.h"
 
 /*----------------------------------------------------------------------------*/
 uint16_t buf2uint16(uint8_t* buffer)
@@ -61,4 +63,41 @@ uint8_t parse_ep_deviceid(uint32_t device_id)
 uint8_t parse_devtype_deviceid(uint32_t device_id)
 {
     return (uint8_t)(device_id);
+}
+
+/*----------------------------------------------------------------------------*/
+const char sw_name[] = "switch";
+const char btn_name[] = "button";
+const char dim_name[] = "dimmer";
+const char lin_sensor_name[] = "linear sensor";
+const char evt_sensor_name[] = "event sensor";
+const char on_off_bulb_name[] = "on/off bulb";
+const char level_bulb_name[] = "level bulb";
+const char rgb_led_name[] = "rgb led";
+const char servo_name[] = "servo";
+
+const char* device_type_to_name(uint8_t device_type)
+{
+    switch (device_type) {
+    case ha_ns::SWITCH:
+        return sw_name;
+    case ha_ns::BUTTON:
+        return btn_name;
+    case ha_ns::DIMMER:
+        return dim_name;
+    case ha_ns::LIN_SENSOR:
+        return lin_sensor_name;
+    case ha_ns::EVT_SENSOR:
+        return evt_sensor_name;
+    case ha_ns::ON_OFF_BULB:
+        return on_off_bulb_name;
+    case ha_ns::LEVEL_BULB:
+        return level_bulb_name;
+    case ha_ns::RGB_LED:
+        return rgb_led_name;
+    case ha_ns::SERVO_SG90:
+        return servo_name;
+    default:
+        return NULL;
+    }
 }
